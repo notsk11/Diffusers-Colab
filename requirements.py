12ee5f2b0856c994b451d9e8700d795e6a3d1e78
notsk11 <153056264+notsk11@users.noreply.github.com>
2024-01-04 17:26:01 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,11 +1,6 @@
-# Requirements
-!pip install -q diffusers xformers transformers accelerate mediapy numba --upgrade
-import torch
-from IPython.display import clear_output
-from diffusers import AutoPipelineForText2Image
-from diffusers import DiffusionPipeline
-import random, sys
-import mediapy as media
-
-gpu = torch.cuda.is_available()
-clear_output()
+diffusers
+xformers
+transformers
+accelerate
+mediapy
+numba
