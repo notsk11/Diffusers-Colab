9b477e74a9b2d5001c7df49cbb5ae02519a1179c
notsk11 <153056264+notsk11@users.noreply.github.com>
2024-01-04 17:19:00 +0530

    Delete run.py

--- run.py
+++
@@ -1,4 +0,0 @@
-from diffusers import DiffusionPipeline
-import torch
-
-DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
