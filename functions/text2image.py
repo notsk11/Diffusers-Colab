fc771f6bc34c8eb2018d411e6d98618851549d1b
notsk11 <153056264+notsk11@users.noreply.github.com>
2024-01-21 23:24:44 +0530

    Update text2image.py

--- functions/text2image.py
+++ functions/text2image.py
@@ -1,43 +1,55 @@
 # /content/functions/text2image.py
-# text2image.py
 import torch
 import random
 import sys
 from PIL import Image
 import numpy as np
 from functions.load import CustomDiffusionLoader
+from diffusers import (
+    EulerDiscreteScheduler,
+    EulerAncestralDiscreteScheduler,
+    LMSDiscreteScheduler,
+    KDPM2DiscreteScheduler,
+    DPMSolverSinglestepScheduler,
+    DPMSolverMultistepScheduler
+)
+
 
 class TextToImageGenerator:
     def __init__(self, model_name="digiplay/Realisian_v5"):
         self.model_loader = CustomDiffusionLoader(model_name=model_name)
 
-    def txt2img(self, prompt, negative_prompt, height, width, num_inference_steps, guidance_scale, seed, model_name):
-      # Seed randomizer
-      if seed == "":
-          seed = random.randint(0, sys.maxsize)
-      else:
-          try:
-              seed = int(seed)
-          except ValueError:
-              print("Invalid input. Please enter a valid number.")
-              seed = None
+    def txt2img(self, prompt, negative_prompt, height, width, num_inference_steps, guidance_scale, seed, model_name, scheduler):
+        # Seed randomizer
+        if seed == "":
+            seed = random.randint(0, sys.maxsize)
+        else:
+            try:
+                seed = int(seed)
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+                seed = None
 
-      generator = torch.Generator("cuda").manual_seed(seed)
-      
-      # Load or get the pipeline
-      self.model_loader.model_name = model_name
-      pipe = self.model_loader.load_pipeline()
-      pipe.safety_checker = None
-      num_inference_steps = int(num_inference_steps)
-      
-      image = pipe(prompt=prompt, negative_prompt=negative_prompt, height=height, width=width,
-                  num_inference_steps=num_inference_steps, guidance_scale=guidance_scale,
-                  generator=generator).images[0]
+        generator = torch.Generator("cuda").manual_seed(seed)
 
-      # Convert image to NumPy array
-      image_np = np.array(image)
+        # Load or get the pipeline
+        self.model_loader.model_name = model_name
+        pipe = self.model_loader.load_pipeline()
+        pipe.safety_checker = None
 
-      # Convert NumPy array to PIL image
-      image_pil = Image.fromarray(image_np)
+        # Set the scheduler
+        pipe.scheduler = eval(scheduler)
 
-      return [image_pil], seed
+        num_inference_steps = int(num_inference_steps)
+
+        image = pipe(prompt=prompt, negative_prompt=negative_prompt, height=height, width=width,
+                     num_inference_steps=num_inference_steps, guidance_scale=guidance_scale,
+                     generator=generator).images[0]
+
+        # Convert image to NumPy array
+        image_np = np.array(image)
+
+        # Convert NumPy array to PIL image
+        image_pil = Image.fromarray(image_np)
+
+        return [image_pil], seed
