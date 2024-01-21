fb409ed6c58b1f38239ac025120ead321765edbd
notsk11 <153056264+notsk11@users.noreply.github.com>
2024-01-21 14:12:11 +0530

    Update and rename models.py to load.py

---
+++ functions/load.py
@@ -0,0 +1,14 @@
+# /content/functions/load.py
+# load.py
+
+from diffusers import DiffusionPipeline
+
+class CustomDiffusionLoader:
+    def __init__(self, model_name="digiplay/Realisian_v5"):
+        self.model_name = model_name
+        self.pipe = None
+
+    def load_pipeline(self):
+        if self.pipe is None:
+            self.pipe = DiffusionPipeline.from_pretrained(self.model_name).to('cuda')
+        return self.pipe
