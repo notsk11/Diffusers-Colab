e633e93df9bfb5eb49c40d57a3f3678c51716558
notsk11 <153056264+notsk11@users.noreply.github.com>
2024-02-11 17:37:41 +0530

    Update style.py

--- functions/style.py
+++ functions/style.py
@@ -17,122 +17,117 @@ css = """
     height: 3em;
 }
 .prompt {
-    position: absolute;
+    width: 1500px !important;
+    height: 100px !important;
     background-color: transparent;
-    left: 260px;
-    top: 595px;
-    max-width: 42em;
-    min-width: 42em !important;
-    height: 14em;
 }
 .neg-prompt {
-    position: absolute;
+    width: 1500px !important;
+    height: 100px !important;
     background-color: transparent;
-    left: 260px;
-    top: 716px;
-    max-width: 42em;
-    min-width: 42em !important;
-    height: 10em;
 }
 .generate {
-    position: absolute;
-    background: orange;
-    left: 847px;
-    top: 605px;
-    max-width: 19em;
-    min-width: 19em !important;
-    height: 5em;
+    width: 350px !important;
+    height: 100px !important;
+    top: 135px !important;
+    position: fixed;
+    background-color: yellow;
 }
 .steps {
-    position: absolute;
-    background-color: tramsparent;
-    left: -13px;
-    top: 10px;
-    max-width: 20.4em;
-    min-width: 20.4em !important;
-    height: 6em;
+    background-color: transparent;
+    position: fixed;
+    left: 460px;
+    top: 310px !important;
+    width: 350px !important;
+    height: 100em !important;
 }
 .samp-meth {
-    position: absolute;
-    background-color: tramsparent;
-    left: -13px;
-    top: 260px;
-    max-width: 20.4em;
-    min-width: 20.4em !important;
-    height: 7em;
+    width: 350px !important;
+    height: 100px !important;
+    background-color: transparent;
+}
+.hiresfix {
+    background-color: transparent;
+    top: -15px !important;
 }
 .b-count {
-    position: absolute;
-    background-color: tramsparent;
-    left: -13px;
-    top: 333px;
-    max-width: 20.4em;
-    min-width: 20.4em !important;
-    height: 6em;
+    background-color: transparent;
+    width: 300px;
+    left: 510px;
+    top: 455px !important;
+    position: fixed;
 }
 .b-size {
-    position: absolute;
-    background-color: tramsparent;
-    left: -13px;
-    top: 411px;
-    max-width: 20.4em;
-    min-width: 20.4em !important;
-    height: 6em;
+    background-color: transparent;
+    width: 300px;
+    left: 510px;
+    top: 521px !important;
+    position: fixed;
 }
 .width {
-    position: absolute;
-    background-color: tramsparent;
-    left: -13px;
-    top: 90px;
-    max-width: 10em;
-    min-width: 10em !important;
-    height: 8em;
+    background-color: transparent;
+    width: 430px;
 }
 .height {
-    position: absolute;
-    background-color: tramsparent;
-    left: 127px;
-    top: 90px;
-    max-width: 10.3em;
-    min-width: 10.3em !important;
-    height: 8em;
+    background-color: transparent;
+    width: 430px;
 }
 .guidance {
-    position: absolute;
-    background-color: tramsparent;
-    left: -13px;
-    top: 190px;
-    max-width: 20.4em;
-    min-width: 20.4em !important;
-    height: 5em;
+    background-color: transparent;
+    width: 790px;
 }
 .inp-seed {
-    position: absolute;
     background-color: transparent;
-    left: 847px;
-    top: 680px;
-    max-width: 22em;
-    min-width: 22em !important;
-    height: 6em;
-    opacity: 1;
+    width: 400px;
 }
 .out-seed {
-    position: absolute;
     background-color: transparent;
-    left: 847px;
-    top: 760px;
-    max-width: 22em;
-    min-width: 22em !important;
-    height: 5em;
-    opacity: 1;
+    width: 300px;
+    left: 510px;
+    top: 684px !important;
+    position: fixed;
 }
 .img-output {
     position: absolute;
     background: gray;
-    left: 274px;
-    top: 10px;
-    max-width: 63em;
-    min-width: 63em !important;
-    height: 42em;
+    left: 882px;
+    top: 0px;
+    width: 1000px !important;
+    height: 440px !important;
+}
+.row_a {
+    width: 1000px !important;
+    height: 100px !important;
+}
+.colm_a1 {
+    position: fixed;
+    width: 1882px !important;
+    height: 300px !important;
+    left: 20px !important;
+    top: 90px;
+}
+.colm_a2 {
+    width: 2em !important;
+    height: 5em;
+    left: 1290px;
+    top: -100px;
+}
+.row_b {
+    width: 1000px !important;
+    height: 10px !important;
+}
+.colm_b1 {
+    position: fixed;
+    width: 800px !important;
+    height: 1200px !important;
+    left: 20px !important;
+    top: 310px !important;
+}
+.colm_b2 {
+    position: fixed;
+    width: 100px !important;
+    height: 800px !important;
+    left: 400px;
+    top: 310px !important;
 }
 """
